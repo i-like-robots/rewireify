ada533264142460721cc058d8331ea3ba8406484
i-like-robots <matt@maketea.co.uk>
2014-04-16 17:55:52 +0100

    Create a list of modules for transform to ignore

--- test/build.js
+++ test/build.js
@@ -3,6 +3,8 @@ var path = require("path");
 var browserify = require("browserify");
 var rewireify = require("../lib/index");
 
+console.log("Building test bundle...");
+
 browserify("./template/module.js", {basedir: __dirname})
   .transform(rewireify)
   .bundle({ standalone: "test-bundle" }, function(err, output) {
